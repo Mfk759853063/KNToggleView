4d89370fd7ae50cb85abae99f913403f1f14e9a8
vbn <zhoucan123@vip.qq.com>
2016-12-01 12:10:07 +0800

    Initial Commit

---
+++ KNToggleView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KNToggleView
+//
+//  Created by vbn on 16/12/1.
+//  Copyright © 2016年 vbn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KNToggleView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KNToggleView
+//
+//  Created by vbn on 16/12/1.
+//  Copyright © 2016年 vbn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
